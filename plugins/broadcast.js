363d70a3efa3a51c72d5bc9f72cb13ccb2397bfc
ArjunSer <99907222+Arjun-ser-ofc@users.noreply.github.com>
2022-03-25 00:09:37 +0530

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chat_\nEstimation complete ${chats.length * 1.5} second`, m)
   for (let id of chats) {
     await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : ' *〔 Wizard-Ser Broadcast 〕*\n\n' + teks + '\n\n' + watermark), true).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : ' *〔 𝚫રյ𝛖𝛈𝑆𝜀ર 𝛣ર𝛐𝛢𝐷𝐶𝛢𝑆𝛵 〕*\n\n' + teks + '\n\n' + watermark), true).catch(_ => _)
   }
   m.reply('_*Broadcast Complete*_')
 }
