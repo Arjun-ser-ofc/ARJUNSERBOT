dbe37e966d5872ac4c6711bc886f265890c7cff8
ArjunSer <99907222+Arjun-ser-ofc@users.noreply.github.com>
2022-03-25 01:00:24 +0530

    CODED BY ARJUNSER

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ for (let file of files) {
   spawn(process.argv0, ['-c', file])
   .on('close', () => {
     assert.ok(file)
-    console.log('Done', fil)
+    console.log('Done', file)
   })
   .stderr.on('data', chunk => assert.ok(chunk.length < 1, file + '\n\n' + chunk))
 }
