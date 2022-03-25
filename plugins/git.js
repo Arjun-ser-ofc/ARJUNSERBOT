fd5c0ad88eccf2043fbcf481fa99a8ae6ff97fdd
ArjunSer <99907222+Arjun-ser-ofc@users.noreply.github.com>
2022-03-26 01:07:21 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,8 +1,9 @@
 let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
-    let devil = `ㅤ
+    let haruno = `┌ 「 𝐆𝐈𝐓」
+     https://github.com/Arjun-ser-ofc/ARJUNSERBOT🙂  𝐅𝛐ર 𝛨𝜀𝐿𝛲 https://wa.me/916238440754 
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/9b98520c4158403cda881.jpg")).buffer(), ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ ғᴏʀ ʜᴇʟᴘ wa.me/916238440754, '  ᴀʀᴊᴜɴsᴇʀ ʙᴏᴛ ʙʏ ᴀʀᴊᴜɴʟᴀʟ\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ https://github.com/Arjun-ser-ofc/ARJUNSERBOT \n\n\ © ī.am ꪶᴀʀᴊᴜɴsᴇʀꫂ⁩⁴⁰⁴⁩' , '❤️', '.❤️', '🙂', ',🙂')
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/9b98520c4158403cda881.jpg")).buffer(), haruno, '© ᴀʀᴊᴜɴsᴇʀ', 'ᴍᴇɴᴜ', '.menu', 'ᴏᴡɴᴇʀ', '.owner', m)
 }
 handler.tags = ['main']
 handler.help = ['git']
