375c6847e9007c01d36f4015c7873cc4e13e7b2c
ArjunSer <99907222+Arjun-ser-ofc@users.noreply.github.com>
2022-03-27 17:28:00 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,7 +1,8 @@
 let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let haruno = `┌ 「 𝐆𝐈𝐓」
-     https://github.com/Arjun-ser-ofc/ARJUNSERBOT 🙂  𝐅𝛐ર 𝛨𝜀𝐿𝛲 https://wa.me/916238440754 ❤
+     https://github.com/Arjun-ser-ofc/ARJUNSERBOT 🙂  
+𝐅𝛐ર 𝛨𝜀𝐿𝛲 https://wa.me/916238440754 ❤
 `.trim()
     await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/9b98520c4158403cda881.jpg")).buffer(), haruno, '© ᴀʀᴊᴜɴsᴇʀ', 'ᴍᴇɴᴜ', '.menu', 'ᴏᴡɴᴇʀ', '.owner', m)
 }
