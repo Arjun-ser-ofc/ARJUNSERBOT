d6ecc8372a6a1e8105a9e645514b36d3d4e53452
ArjunSer <99907222+Arjun-ser-ofc@users.noreply.github.com>
2022-03-30 03:29:30 +0530

    Update Ariel.js

--- plugins/Ariel.js
+++ plugins/Ariel.js
@@ -6,7 +6,7 @@ Dont Tag My Master...
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@919744196187/i
+handler.customPrefix = /@916238440754/i
 handler.command = new RegExp
 
 module.exports = handler
